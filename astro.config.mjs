30d96ad24a4a7ab4c03af0a7ea0dd800ddb9c678
Adrismate <madroalmondro@gmail.com>
2025-02-19 15:37:43 +0100

    astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://Adrismate.github.io'
+    site: 'https://Adrismate.github.io',
+    base: '/adrismate-website'
 });
