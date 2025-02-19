9b15859a82526ce57e580e591748797756f8762f
Adrismate <madroalmondro@gmail.com>
2025-02-19 20:38:52 +0100

    deploying in cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://adrismate.github.io',
-    base: 'adrismate-website',
+    base: 'adrismate-website-astro',
 });
